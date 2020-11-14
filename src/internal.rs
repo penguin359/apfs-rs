b0ab11ec70d678de2574e4b30e637200bf3ecceb
Loren M. Lang <lorenl@north-winds.org>
2020-11-14 13:06:15 -0800

    Changed to using dedicated struct types for Paddr, Oid, and Xid

--- src/internal.rs
+++ src/internal.rs
@@ -5,7 +5,8 @@ use std::io::{self, prelude::*};
 //use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use byteorder::{LittleEndian, ReadBytesExt};
 
-type Paddr = i64;
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Paddr(i64);
 
 struct Prange {
     pr_start_paddr: Paddr,
@@ -15,7 +16,7 @@ struct Prange {
 impl Prange {
     fn import(source: &mut dyn Read) -> io::Result<Self> {
         Ok(Self {
-            pr_start_paddr: source.read_i64::<LittleEndian>()?,
+            pr_start_paddr: Paddr(source.read_i64::<LittleEndian>()?),
             pr_block_count: source.read_u64::<LittleEndian>()?,
         })
     }
@@ -43,8 +44,11 @@ fn import_uuid(source: &mut dyn Read) -> io::Result<Uuid> {
     ])
 }
 
-type Oid = u64;
-type Xid = u64;
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Oid(u64);
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Xid(u64);
 
 
 const MAX_CKSUM_SIZE: usize = 8;
@@ -62,8 +66,8 @@ impl ObjPhys {
     fn import(source: &mut dyn Read) -> io::Result<Self> {
         Ok(Self {
             o_cksum: source.read_u64::<LittleEndian>()?,
-            o_oid: source.read_u64::<LittleEndian>()?,
-            o_xid: source.read_u64::<LittleEndian>()?,
+            o_oid: Oid(source.read_u64::<LittleEndian>()?),
+            o_xid: Xid(source.read_u64::<LittleEndian>()?),
             o_type: source.read_u32::<LittleEndian>()?,
             o_subtype: source.read_u32::<LittleEndian>()?,
         })
@@ -134,10 +138,10 @@ const OBJECT_TYPE_FLAGS_DEFINED_MASK: u32 = 0xf8000000;
 //
 //
 //typedef enum {
-//	NX_CNTR_OBJ_CKSUM_SET = 0,
-//	NX_CNTR_OBJ_CKSUM_FAIL = 1,
+//      NX_CNTR_OBJ_CKSUM_SET = 0,
+//      NX_CNTR_OBJ_CKSUM_FAIL = 1,
 //
-//	NX_NUM_COUNTERS = 32
+//      NX_NUM_COUNTERS = 32
 //} nx_counter_id_t;
 //
 //
@@ -152,53 +156,53 @@ const NX_EPH_INFO_COUNT: usize = 4;
 //#define NX_EPH_INFO_VERSION_1 1
 
 struct NxSuperblock {
-	nx_o: ObjPhys,
-	nx_magic: u32,
-	nx_block_size: u32,
-	nx_block_count: u64,
+        nx_o: ObjPhys,
+        nx_magic: u32,
+        nx_block_size: u32,
+        nx_block_count: u64,
 
-	nx_features: u64,
-	nx_readonly_compatible_features: u64,
-	nx_incompatible_features: u64,
+        nx_features: u64,
+        nx_readonly_compatible_features: u64,
+        nx_incompatible_features: u64,
 
-	nx_uuid: Uuid,
+        nx_uuid: Uuid,
 
-	nx_next_oid: Oid,
-	nx_next_xid: Xid,
+        nx_next_oid: Oid,
+        nx_next_xid: Xid,
 
-	nx_xp_desc_blocks: u32,
-	nx_xp_data_blocks: u32,
-	nx_xp_desc_base: Paddr,
-	nx_xp_data_base: Paddr,
-	nx_xp_desc_next: u32,
-	nx_xp_data_next: u32,
-	nx_xp_desc_index: u32,
-	nx_xp_desc_len: u32,
-	nx_xp_data_index: u32,
-	nx_xp_data_len: u32,
+        nx_xp_desc_blocks: u32,
+        nx_xp_data_blocks: u32,
+        nx_xp_desc_base: Paddr,
+        nx_xp_data_base: Paddr,
+        nx_xp_desc_next: u32,
+        nx_xp_data_next: u32,
+        nx_xp_desc_index: u32,
+        nx_xp_desc_len: u32,
+        nx_xp_data_index: u32,
+        nx_xp_data_len: u32,
 
-	nx_spaceman_oid: Oid,
-	nx_omap_oid: Oid,
-	nx_reaper_oid: Oid,
+        nx_spaceman_oid: Oid,
+        nx_omap_oid: Oid,
+        nx_reaper_oid: Oid,
 
-	nx_test_type: u32,
+        nx_test_type: u32,
 
-	nx_max_file_systems: u32,
-	nx_fs_oid: [Oid; NX_MAX_FILE_SYSTEMS],
-	nx_counters: [u64; NX_NUM_COUNTERS],
-	nx_blocked_out_prange: Prange,
-	nx_evict_mapping_tree_oid: Oid,
-	nx_flags: u64,
-	nx_efi_jumpstart: Paddr,
-	nx_fusion_uuid: Uuid,
-	nx_keylocker: Prange,
-	nx_ephemeral_info: [u64; NX_EPH_INFO_COUNT],
+        nx_max_file_systems: u32,
+        nx_fs_oid: [Oid; NX_MAX_FILE_SYSTEMS],
+        nx_counters: [u64; NX_NUM_COUNTERS],
+        nx_blocked_out_prange: Prange,
+        nx_evict_mapping_tree_oid: Oid,
+        nx_flags: u64,
+        nx_efi_jumpstart: Paddr,
+        nx_fusion_uuid: Uuid,
+        nx_keylocker: Prange,
+        nx_ephemeral_info: [u64; NX_EPH_INFO_COUNT],
 
-	nx_test_oid: Oid,
+        nx_test_oid: Oid,
 
-	nx_fusion_mt_oid: Oid,
-	nx_fusion_wbc_oid: Oid,
-	nx_fusion_wbc: Prange,
+        nx_fusion_mt_oid: Oid,
+        nx_fusion_wbc_oid: Oid,
+        nx_fusion_wbc: Prange,
 }
 
 impl NxSuperblock {
@@ -215,13 +219,13 @@ impl NxSuperblock {
 
             nx_uuid: import_uuid(source)?,
 
-            nx_next_oid: source.read_u64::<LittleEndian>()?,
-            nx_next_xid: source.read_u64::<LittleEndian>()?,
+            nx_next_oid: Oid(source.read_u64::<LittleEndian>()?),
+            nx_next_xid: Xid(source.read_u64::<LittleEndian>()?),
 
             nx_xp_desc_blocks: source.read_u32::<LittleEndian>()?,
             nx_xp_data_blocks: source.read_u32::<LittleEndian>()?,
-            nx_xp_desc_base: source.read_i64::<LittleEndian>()?,
-            nx_xp_data_base: source.read_i64::<LittleEndian>()?,
+            nx_xp_desc_base: Paddr(source.read_i64::<LittleEndian>()?),
+            nx_xp_data_base: Paddr(source.read_i64::<LittleEndian>()?),
             nx_xp_desc_next: source.read_u32::<LittleEndian>()?,
             nx_xp_data_next: source.read_u32::<LittleEndian>()?,
             nx_xp_desc_index: source.read_u32::<LittleEndian>()?,
@@ -229,27 +233,27 @@ impl NxSuperblock {
             nx_xp_data_index: source.read_u32::<LittleEndian>()?,
             nx_xp_data_len: source.read_u32::<LittleEndian>()?,
 
-            nx_spaceman_oid: source.read_u64::<LittleEndian>()?,
-            nx_omap_oid: source.read_u64::<LittleEndian>()?,
-            nx_reaper_oid: source.read_u64::<LittleEndian>()?,
+            nx_spaceman_oid: Oid(source.read_u64::<LittleEndian>()?),
+            nx_omap_oid: Oid(source.read_u64::<LittleEndian>()?),
+            nx_reaper_oid: Oid(source.read_u64::<LittleEndian>()?),
 
             nx_test_type: source.read_u32::<LittleEndian>()?,
 
             nx_max_file_systems: source.read_u32::<LittleEndian>()?,
-            nx_fs_oid: [0; 100], //[Oid; NX_MAX_FILE_SYSTEMS],
+            nx_fs_oid: [Oid(0); 100], //[Oid; NX_MAX_FILE_SYSTEMS],
             nx_counters: [0; 32], //[u64; NX_NUM_COUNTERS],
             nx_blocked_out_prange: Prange::import(source)?,
-            nx_evict_mapping_tree_oid: source.read_u64::<LittleEndian>()?,
+            nx_evict_mapping_tree_oid: Oid(source.read_u64::<LittleEndian>()?),
             nx_flags: source.read_u64::<LittleEndian>()?,
-            nx_efi_jumpstart: source.read_i64::<LittleEndian>()?,
+            nx_efi_jumpstart: Paddr(source.read_i64::<LittleEndian>()?),
             nx_fusion_uuid: import_uuid(source)?,
             nx_keylocker: Prange::import(source)?,
             nx_ephemeral_info: [0; 4], //[u64; NX_EPH_INFO_COUNT],
 
-            nx_test_oid: source.read_u64::<LittleEndian>()?,
+            nx_test_oid: Oid(source.read_u64::<LittleEndian>()?),
 
-            nx_fusion_mt_oid: source.read_u64::<LittleEndian>()?,
-            nx_fusion_wbc_oid: source.read_u64::<LittleEndian>()?,
+            nx_fusion_mt_oid: Oid(source.read_u64::<LittleEndian>()?),
+            nx_fusion_wbc_oid: Oid(source.read_u64::<LittleEndian>()?),
             nx_fusion_wbc: Prange::import(source)?,
         })
     }
@@ -283,29 +287,69 @@ impl NxSuperblock {
 //
 //
 //struct checkpoint_mapping {
-//	uint32_t cpm_type;
-//	uint32_t cpm_subtype;
-//	uint32_t cpm_size;
-//	uint32_t cpm_pad;
-//	oid_t cpm_fs_oid;
-//	oid_t cpm_oid;
-//	oid_t cpm_paddr;
+//      uint32_t cpm_type;
+//      uint32_t cpm_subtype;
+//      uint32_t cpm_size;
+//      uint32_t cpm_pad;
+//      oid_t cpm_fs_oid;
+//      oid_t cpm_oid;
+//      oid_t cpm_paddr;
 //};
 //typedef struct checkpoint_mapping checkpoint_mapping_t;
 //
 //
 //struct checkpoint_map_phys {
-//	obj_phys_t cpm_o;
-//	uint32_t cpm_flags;
-//	uint32_t cpm_count;
-//	checkpoint_mapping_t cpm_map[];
+//      obj_phys_t cpm_o;
+//      uint32_t cpm_flags;
+//      uint32_t cpm_count;
+//      checkpoint_mapping_t cpm_map[];
 //};
 //
 //#define CHECKPOINT_MAP_LAST 0x00000001
 //
 //
 //struct evict_mapping_val {
-//	paddr_t dst_paddr;
-//	uint64_t len;
+//      paddr_t dst_paddr;
+//      uint64_t len;
 //} __attribute__((packed));
 //typedef struct evict_mapping_val evict_mapping_val_t;
+//
+//
+
+#[repr(u32)]
+enum ObjectType {
+    NxSuperblock        = 0x00000001,
+    Btree               = 0x00000002,
+    BtreeNode           = 0x00000003,
+    Spaceman            = 0x00000005,
+    SpacemanCab         = 0x00000006,
+    SpacemanCib         = 0x00000007,
+    SpacemanBitmap      = 0x00000008,
+    SpacemanFreeQueue   = 0x00000009,
+    ExtentListTree      = 0x0000000a,
+    Omap                = 0x0000000b,
+    CheckpointMap       = 0x0000000c,
+    Fs                  = 0x0000000d,
+    Fstree              = 0x0000000e,
+    Blockreftree        = 0x0000000f,
+    Snapmetatree        = 0x00000010,
+    NxReaper            = 0x00000011,
+    NxReapList          = 0x00000012,
+    OmapSnapshot        = 0x00000013,
+    EfiJumpstart        = 0x00000014,
+    FusionMiddleTree    = 0x00000015,
+    NxFusionWbc         = 0x00000016,
+    NxFusionWbcList     = 0x00000017,
+    ErState             = 0x00000018,
+    Gbitmap             = 0x00000019,
+    GbitmapTree         = 0x0000001a,
+    GbitmapBlock        = 0x0000001b,
+    ErRecoveryBlock     = 0x0000001c,
+    SnapMetaExt         = 0x0000001d,
+    IntegrityMeta       = 0x0000001e,
+    FextTree            = 0x0000001f,
+    Reserved20          = 0x00000020,
+
+    Invalid             = 0x00000000,
+    Test                = 0x000000ff,
+}
